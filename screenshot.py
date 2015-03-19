2262a53fc2882706ecc79e5ac6620636d4135596
Samael500 <samael500@gmail.com>
2015-03-19 18:47:28 +0300

    :rocket: report log

--- screenshot.py
+++ screenshot.py
@@ -6,9 +6,12 @@ import time
 
 URL_LIST = ('http://google.com/', 'https://www.facebook.com/', 'http://habrahabr.ru/', 'http://wbtech.pro/', )
 RES_LIST = (240, 780, 1320, 1920)
+ATTEMPTS = 10
 
 get_img_name = lambda base_path, url, res: '{base_path}/{url}-{res}px.png'.format(
     base_path=base_path, url=url.replace('http:', '').replace('https:', '').replace('/', ''), res=res)
+
+avg = lambda arr: sum(arr) / len(arr)
 
 
 def save_shot(url, width, browser, save_as):
@@ -24,25 +27,48 @@ def _test_browser(Browser, url, res, save_as):
     save_shot(url, res, browser, save_as)
     browser.quit()
 
+def _report(browser_name, save_as, mins, maxs, avgs, times):
+    # make report
+    with open(browser_name + '.log', 'a') as report:
+        report.write(save_as + '\n')
+        report.write('min: {value}\n'.format(value=min(mins)))
+        report.write('max: {value}\n'.format(value=max(maxs)))
+        report.write('avg: {value}\n'.format(value=avg(avgs)))
+        report.write('time: {value}\n'.format(value=avg(times)))
+        report.write('\n')
+    with open(browser_name + '-min.log', 'a') as report:
+        report.write('{0};{1};{2};{3}\n'.format(min(mins), max(maxs), avg(avgs), avg(times)))
 
-def test_browser(Browser):
+def test_browser(Browser, browser_name):
     """ for each site, for each screen """
     # base path
-    browser = Browser()
-    base_path = os.path.join('img', browser.name)
+    base_path = os.path.join('img', browser_name)
     if not os.path.exists(base_path):
         os.makedirs(base_path)
-    browser.quit()
+
+    # clear report
+    with open(browser_name + '.log', 'w') as report:
+        report.write('')
+    with open(browser_name + '-min.log', 'w') as report:
+        report.write('')
 
     for url in URL_LIST:
         for res in RES_LIST:
             save_as = get_img_name(base_path, url, res)
-            start = time.time()
-            memory = memory_usage((_test_browser, (Browser, url, res, save_as)))
-            end = time.time()
-            print 'time:', end - start
-            print 'min:', min(memory), 'max:', max(memory), 'avg:', sum(memory) / len(memory)
+            mins = []
+            maxs = []
+            avgs = []
+            times = []
+            for i in xrange(ATTEMPTS):
+                start = time.time()
+                memory = memory_usage((_test_browser, (Browser, url, res, save_as)))
+                end = time.time()
+                mins.append(min(memory))
+                maxs.append(max(memory))
+                avgs.append(avg(memory))
+                times.append(end - start)
+            _report(browser_name, save_as, mins, maxs, avgs, times)
 
 
 if __name__ == '__main__':
-    test_browser(webdriver.Firefox)
+    test_browser(webdriver.Firefox, 'firefox')
