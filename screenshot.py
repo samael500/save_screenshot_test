13a6ba87e3202d400e6a3853beea77c592f31693
Samael500 <samael500@gmail.com>
2015-03-19 14:15:45 +0300

    :repeat: firefox test

--- screenshot.py
+++ screenshot.py
@@ -1,28 +1,32 @@
 from selenium import webdriver
 from pyvirtualdisplay import Display
+import os
 
 URL_LIST = ('http://google.com/', 'https://www.facebook.com/', 'http://habrahabr.ru/', 'http://wbtech.pro/', )
 RES_LIST = (240, 780, 1320, 1920)
-get_img_name = lambda browser, url, res: 'img/{browser}/{url}-{res}px.png'.format(
-    browser=browser.name, url=url.replace('http:', '').replace('https:', '').replace('/', ''), res=res)
 
+get_img_name = lambda base_path, url, res: '{base_path}/{url}-{res}px.png'.format(
+    base_path=base_path, url=url.replace('http:', '').replace('https:', '').replace('/', ''), res=res)
 
 def save_shot(url, width, browser, save_as):
     """ Open page and save screenshot """
     browser.set_window_size(width, 768)
-    browser.set_page_load_timeout(60)
     browser.get(url)
     browser.save_screenshot(save_as)
-    browser.quit()
 
 def test_browser(browser):
     """ for each site, for each screen """
-    print browser.name
+    base_path = os.path.join('img', browser.name)
+    if not os.path.exists(base_path):
+        os.makedirs(base_path)
 
     for url in URL_LIST:
         for res in RES_LIST:
-            save_as = get_img_name(browser, url, res)
-            save_shot(url, res, browser)
+            save_as = get_img_name(base_path, url, res)
+            save_shot(url, res, browser, save_as)
+
+    browser.quit()
+
 
 if __name__ == '__main__':
     browser = webdriver.Firefox()
