e8ebb0bb36cf037089f83aed53ac823cf91ee4c7
Samael500 <samael500@gmail.com>
2015-03-23 21:19:04 +0300

    :rocket: splash test

--- splash_base.py
+++ splash_base.py
@@ -4,11 +4,13 @@ from helpers import test_browser
 
 def splash(foo_none1, url, res, save_as, boo_none2):
     """ create rowser and save img """
-    request = 'http://localhost:8050/render.png?url={url}&width={res}&timeout=10'.format(url=url, res=res)
-    res = subprocess.check_call(['curl', request, '>', save_as])
+    request = 'http://localhost:8050/render.png?url={url}&width={res}&timeout=60'.format(url=url, res=res)
+    res = subprocess.check_call(['curl', request, '-o', save_as])
     assert res == 0
 
 
 def splash_test_browser(name):
-    if name is 'splash':
+    if name == 'splash':
         test_browser(None, name, None, splash)
+    else:
+        assert False
