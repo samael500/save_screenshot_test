f55c8072d626cd255396e4cb75223a8b8b732930
Samael500 <samael500@gmail.com>
2015-03-25 22:56:35 +0300

    :repeat: phantom subprocess memory usage

--- phantomjs_base.py
+++ phantomjs_base.py
@@ -1,6 +1,10 @@
-from helpers import test_browser
+from memory_profiler import memory_usage
+from helpers import test_browser_extra_mem
+
 import subprocess
 import os
+import time
+
 
 script_path = 'phantom_screen.js'
 
@@ -18,17 +22,22 @@ def make_param(url, res, save_as):
     with open(script_path, 'w') as phantomjs:
         phantomjs.write(script)
 
-def no_vdisplay_test_browser(path, url, res, save_as, none_2):
+def no_vdisplay_test_browser(path, url, res, save_as, none_2, memory):
     """ create rowser and save img """
     # virtual display
     make_param(url, res, save_as)
-    res = subprocess.check_call([path, script_path, '--ssl-protocol=any'])
+    p = subprocess.Popen([path, script_path, '--ssl-protocol=any'])
+    while p.poll() is None:
+        memory.extend(memory_usage(p.pid))
+        time.sleep(0.01)
+    res = p.poll()
+
     assert res == 0
 
 def phantomjs_test_browser(name):
 
     if name == 'phantomjs-no_selenium':
-        test_browser('./bin/phantomjs-1.9.8-linux-x86_64/bin/phantomjs', name, None, no_vdisplay_test_browser)
+        test_browser_extra_mem('./bin/phantomjs-1.9.8-linux-x86_64/bin/phantomjs', name, None, no_vdisplay_test_browser)
 
     if name == 'phantomjs2-no_selenium':
-        test_browser('./bin/phantomjs-2.0.0/bin/phantomjs', name, None, no_vdisplay_test_browser)
+        test_browser_extra_mem('./bin/phantomjs-2.0.0/bin/phantomjs', name, None, no_vdisplay_test_browser)
