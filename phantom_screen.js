52712f1dd102d29176db9152b6c16468dbb7db95
Samael500 <samael500@gmail.com>
2015-03-25 15:03:01 +0300

    :rocket: global retest

---
+++ phantom_screen.js
@@ -0,0 +1,8 @@
+
+        var page = require('webpage').create();
+        page.settings.userAgent = 'Mozilla/5.0 (X11; Linux x86_64) Gecko/20100101 Firefox/36.0';
+        page.viewportSize = { width:1920, height:768 };
+        page.open('http://wbtech.pro/', function (status) {
+            page.render('img/phantomjs2-no_selenium/wbtech.pro-1920px.png');
+            phantom.exit();
+        });
