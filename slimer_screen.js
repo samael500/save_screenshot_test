e3adc938186b6887120455040d585f90de032a8b
Samael500 <samael500@gmail.com>
2015-03-27 18:36:57 +0300

    :repeat: retest

--- slimer_screen.js
+++ slimer_screen.js
@@ -4,7 +4,7 @@
         page.settings.userAgent = 'Mozilla/5.0 (X11; Linux x86_64) Gecko/20100101 Firefox/36.0';
         page.open('http://wbtech.pro/', function (status) {
             page.viewportSize = { width:1920, height:768 };
-            page.render('img/slimerjs/wbtech.pro-1920px.png');
+            page.render('img/slimerjs10/wbtech.pro-1920px.png');
             page.close();
             slimer.exit();
         });
